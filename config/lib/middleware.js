b0bf28428e727033856895bfe680b1332831d3f3
John Conrad Lumacad <jcmlumacad@tmj.ph>
2016-11-29 11:01:33 +0800

    add express session

--- config/lib/middleware.js
+++ config/lib/middleware.js
@@ -3,11 +3,17 @@
 var csrf = require('csurf'),
     csrfProtection = csrf({ cookie: true }),
     helmet = require('helmet'),
+    session = require('express-session'),
     passport = require('passport');
 
 module.exports = function(app) {
     app.use(csrfProtection);
     app.use(helmet());
+    app.use(session({
+        secret: process.env.SESSION_SECRET || 'secret',
+        resave: false,
+        saveUninitialized: true
+    }));
     app.use(passport.initialize());
     app.use(passport.session());
 };
