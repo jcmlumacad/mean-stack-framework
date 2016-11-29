47d7755f52612d3d477c88f1de9b4b5a40bfbe54
John Conrad Lumacad <jcmlumacad@tmj.ph>
2016-11-29 10:59:42 +0800

    update middleware

--- config/lib/middleware.js
+++ config/lib/middleware.js
@@ -2,9 +2,12 @@
 
 var csrf = require('csurf'),
     csrfProtection = csrf({ cookie: true }),
-    helmet = require('helmet');
+    helmet = require('helmet'),
+    passport = require('passport');
 
 module.exports = function(app) {
     app.use(csrfProtection);
     app.use(helmet());
+    app.use(passport.initialize());
+    app.use(passport.session());
 };
