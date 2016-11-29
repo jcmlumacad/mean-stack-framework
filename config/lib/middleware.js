8d468a493951e0ccc4a78bc53589727e5c3fe6bf
John Conrad Lumacad <jcmlumacad@tmj.ph>
2016-11-29 12:11:22 +0800

    add body parser

--- config/lib/middleware.js
+++ config/lib/middleware.js
@@ -3,12 +3,15 @@
 var csrf = require('csurf'),
     csrfProtection = csrf({ cookie: true }),
     helmet = require('helmet'),
+    bodyParser = require('body-parser'),
     session = require('express-session'),
     passport = require('passport');
 
 module.exports = function(app) {
     app.use(csrfProtection);
     app.use(helmet());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
     app.use(session({
         secret: process.env.SESSION_SECRET || 'secret',
         resave: false,
