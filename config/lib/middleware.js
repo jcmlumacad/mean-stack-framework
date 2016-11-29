970ce2b7b3a834593062c72469b575bb4f47eb8a
John Conrad Lumacad <jcmlumacad@tmj.ph>
2016-11-29 12:18:49 +0800

    add cookie parser and method override

--- config/lib/middleware.js
+++ config/lib/middleware.js
@@ -1,13 +1,16 @@
 'use strict';
 
 var csrf = require('csurf'),
+    cookieParser = require('cookie-parser'),
     csrfProtection = csrf({ cookie: true }),
     helmet = require('helmet'),
     bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
     session = require('express-session'),
     passport = require('passport');
 
 module.exports = function(app) {
+    app.use(cookieParser(Math.random().toString(36).substring(7)));
     app.use(csrfProtection);
     app.use(helmet());
     app.use(bodyParser.urlencoded({ extended: true }));
