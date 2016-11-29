4e99ae629fc6ea470a248e0f711c3df278ca75db
John Conrad Lumacad <jcmlumacad@tmj.ph>
2016-11-29 10:58:14 +0800

    add helmet

--- config/lib/middleware.js
+++ config/lib/middleware.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var csrf = require('csurf'),
-    csrfProtection = csrf({ cookie: true });
+    csrfProtection = csrf({ cookie: true }),
+    helmet = require('helmet');
 
 module.exports = function(app) {
     app.use(csrfProtection);
+    app.use(helmet());
 };
