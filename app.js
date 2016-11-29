c14911c6b89661b3194dcb71e58e01ea352e0d9f
John Conrad Lumacad <jcmlumacad@tmj.ph>
2016-11-29 09:46:56 +0800

    require env

--- app.js
+++ app.js
@@ -3,4 +3,7 @@
 var express = require('express'),
     app = expresss();
 
+// Load environment properties from a .env file for local development
+require('dotenv').load({ silent: true });
+
 module.exports = app;
