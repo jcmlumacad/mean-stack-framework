e43de315b4106dbe6e94f512ac3e2378764e6c74
John Conrad Lumacad <jcmlumacad@tmj.ph>
2016-12-09 10:18:29 +0800

    Update middleware.js

--- config/lib/middleware.js
+++ config/lib/middleware.js
@@ -4,6 +4,8 @@ var csrf = require('csurf'),
     cookieParser = require('cookie-parser'),
     csrfProtection = csrf({ cookie: true }),
     helmet = require('helmet'),
+    methodOverride = require('method-override'),
+    bodyParser = require('body-parser'),
     session = require('express-session'),
     passport = require('passport');
 
@@ -11,6 +13,9 @@ module.exports = function(app) {
     app.use(cookieParser(Math.random().toString(36).substring(7)));
     app.use(csrfProtection);
     app.use(helmet());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(methodOverride('_method'));
     app.use(session({
         secret: process.env.SESSION_SECRET || 'secret',
         resave: false,
