2a1a5675e971c602f68f138e99b921135f9ed232
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-06-08 16:44:16 +0800

    enhanced mean stack

--- config/lib/passport.js
+++ config/lib/passport.js
@@ -23,8 +23,8 @@ module.exports = function (app) {
     }, function (email, password, done) {
         User.findOne({ email: email }, function (err, user) {
             if (err) { return done(err); }
-            if ( ! user) { return done(null, false); }
-            if ( ! user.verifyPassword(password)) { return done(null, false); }
+            if (!user) { return done(null, false); }
+            if (!user.verifyPassword(password)) { return done(null, false); }
             return done(null, user);
         });
     }));
